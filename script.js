d4b12236e594ff1295ceeaee2affba35e2233397
vibha-vs <vibhavs217@gmail.com>
2021-12-05 01:35:27 +0530

    Validating the signup form

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-// Remove Items From Cart
-$('a.remove').click(function(){
-    event.preventDefault();
-    $( this ).parent().parent().parent().hide( 400 );
-   
-  })
-  
-  // Just for testing, show all items
-    $('a.btn.continue').click(function(){
-      $('li.items').show(400);
-    })
-  
-    
+
+
+function loginfunc() {
+
+  var validmail = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-.]+(?:\.[a-zA-Z0-9-]+)$/;
+
+  if (!(document.getElementById("email").value.match(validmail))) {
+
+    alert("Invalid email address!");
+
+    document.getElementById("email").focus();
+  }
+  else if(document.getElementById("psw").value.length==0)
+  {
+    alert("Password cannot be blank!");
+    document.getElementById("psw").focus();
+    return false;
+  }
+  else return true;
+}
+function signupfunc() {
+
+  var validpw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*s).{8,15}$/;
+  var validmail = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-.]+(?:\.[a-zA-Z0-9-]+)$/;
+
+  if (!(document.getElementById("mail").value.match(validmail))) {
+
+    alert("Invalid email address!");
+
+    document.getElementById("mail").focus();
+  }
+ else if(document.getElementById("pw").value.length==0){
+   alert("Password cannot be blank!");
+   document.getElementById("pw").focus();
+ }
+
+  else if (!(document.getElementById("pw").value.match(validpw))) {
+    alert("Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character and it should be 8-15 characters long.")
+    document.getElementById("pw").focus();
+
+
+  }
+  else if ((document.getElementById("pw").value) != (document.getElementById("cpw").value)) {
+    alert("Passwords do not match!");
+    document.getElementById("cpw").focus();
+
+  }
+else 
+alert("You are signed in!");
+}
